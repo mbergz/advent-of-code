89f774e0eea677faa83b5b6db4a6b27cf733659f
Martin Bergström <emarbe02@gmail.com>
2024-09-11 23:02:06 +0200

    Day3 completed

--- day03/day3.go
+++ day03/day3.go
@@ -13,6 +13,13 @@ type Coord struct {
 	y int
 }
 
+var directions = map[byte]Coord{
+	'R': {1, 0},
+	'D': {0, 1},
+	'L': {-1, 0},
+	'U': {0, -1},
+}
+
 func main() {
 	input, err := os.ReadFile("./day3.txt")
 	if err != nil {
@@ -26,59 +33,55 @@ func part1(input string) {
 	visitedFirstWire := createVisitedFromWire(strings.Split(input, "\n")[0])
 	visitedSecondWire := createVisitedFromWire(strings.Split(input, "\n")[1])
 
-	var intersecting []Coord
+	var lowestManhattan float64 = math.MaxFloat64
 
 	for key := range visitedFirstWire {
 		_, ok := visitedSecondWire[key]
 		if ok {
-			intersecting = append(intersecting, key)
-		}
-	}
+			lowestManhattan = math.Min(float64(lowestManhattan), manhattanDist(key))
 
-	var lowestManhattan float64 = math.MaxFloat64
-	for _, coord := range intersecting {
-		lowestManhattan = math.Min(float64(lowestManhattan), manhattanDist(coord))
+		}
 	}
 
 	fmt.Println(lowestManhattan)
 }
 
-func createVisitedFromWire(wire string) map[Coord]bool {
-	visited := make(map[Coord]bool)
+func part2(input string) {
+	visitedFirstWire := createVisitedFromWire(strings.Split(input, "\n")[0])
+	visitedSecondWire := createVisitedFromWire(strings.Split(input, "\n")[1])
+
+	var lowestCombinedDist float64 = math.MaxFloat64
+
+	for key, valueFirst := range visitedFirstWire {
+		valueSecond, ok := visitedSecondWire[key]
+		if ok {
+			lowestCombinedDist = math.Min(lowestCombinedDist, float64(valueFirst+valueSecond))
+		}
+	}
+
+	fmt.Println(lowestCombinedDist)
+}
+
+func createVisitedFromWire(wire string) map[Coord]int {
+	visited := make(map[Coord]int)
 
 	current := Coord{0, 0}
+	currentDist := 0
 	for _, cmd := range strings.Split(strings.TrimSpace(wire), ",") {
 		dist := cmd[1:]
-		switch cmd[0] {
-		case 'R':
-			for i := 1; i <= util.ToInt(dist); i++ {
-				current.x = current.x + 1
-				visited[current] = true
-			}
-		case 'D':
-			for i := 1; i <= util.ToInt(dist); i++ {
-				current.y = current.y + 1
-				visited[current] = true
-			}
-		case 'L':
-			for i := 1; i <= util.ToInt(dist); i++ {
-				current.x = current.x - 1
-				visited[current] = true
-			}
-		case 'U':
-			for i := 1; i <= util.ToInt(dist); i++ {
-				current.y = current.y - 1
-				visited[current] = true
-			}
+		delta := directions[cmd[0]]
+
+		for i := 1; i <= util.ToInt(dist); i++ {
+			current.x = current.x + delta.x
+			current.y = current.y + delta.y
+			visited[current] = currentDist + i
 		}
+
+		currentDist += int(math.Abs(float64(util.ToInt(dist))))
 	}
 	return visited
 }
 
 func manhattanDist(coord Coord) float64 {
 	return math.Abs(float64(0-coord.x)) + math.Abs(float64(0-coord.y))
-}
-
-func part2(input string) {
-
 }
