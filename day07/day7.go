4d794e901b585d95813f631eaadd92ddaad7ac2b
Martin Bergström <emarbe02@gmail.com>
2024-09-24 21:59:49 +0200

    Day7 completed

--- day07/day7.go
+++ day07/day7.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -54,6 +55,61 @@ func part1(input string) {
 		max = math.Max(float64(outputE), float64(max))
 	}
 
+	fmt.Println(int(max))
+}
+
+func part2(input string) {
+	defer util.Timer()()
+
+	basePhaseSettings := []int{5, 6, 7, 8, 9}
+	var result [][]int
+	generatePermuations(len(basePhaseSettings), basePhaseSettings, &result)
+
+	var max float64 = 0
+	var wg sync.WaitGroup
+
+	chA := make(chan int)
+	chB := make(chan int)
+	chC := make(chan int)
+	chD := make(chan int)
+	chE := make(chan int)
+
+	for _, setting := range result {
+		wg.Add(5)
+
+		go func() {
+			defer wg.Done()
+			runIntCodeProgramPart2(input, setting[0], chE, chA)
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				lastValue := <-chE
+				max = math.Max(float64(lastValue), float64(max))
+			}()
+		}()
+		go func() {
+			defer wg.Done()
+			runIntCodeProgramPart2(input, setting[1], chA, chB)
+		}()
+		go func() {
+			defer wg.Done()
+			runIntCodeProgramPart2(input, setting[2], chB, chC)
+		}()
+		go func() {
+			defer wg.Done()
+			runIntCodeProgramPart2(input, setting[3], chC, chD)
+		}()
+		go func() {
+			defer wg.Done()
+			runIntCodeProgramPart2(input, setting[4], chD, chE)
+		}()
+
+		go func() {
+			chE <- 0
+		}()
+
+		wg.Wait()
+	}
 	fmt.Println(int(max))
 }
 
@@ -121,8 +177,67 @@ main:
 	panic("Should not happen!")
 }
 
-func part2(input string) {
-	defer util.Timer()()
+func runIntCodeProgramPart2(input string, phaseSetting int, inputChannel chan int, outputChannel chan int) {
+	intArr := createIntArrFromInput(input)
+	firstRun := true
+
+main:
+	for i := 0; i < len(intArr); {
+		opCode := getOpCode(intArr[i])
+		paramModes := getParameterModes(intArr[i])
+
+		switch opCode {
+		case 1:
+			intArr[intArr[i+3]] = getValueByMode(intArr, i+1, paramModes[0]) + getValueByMode(intArr, i+2, paramModes[1])
+			i += 4
+		case 2:
+			intArr[intArr[i+3]] = getValueByMode(intArr, i+1, paramModes[0]) * getValueByMode(intArr, i+2, paramModes[1])
+			i += 4
+		case 3:
+			if firstRun {
+				intArr[intArr[i+1]] = phaseSetting
+				firstRun = false
+			} else {
+				var receivedInput int = <-inputChannel
+				intArr[intArr[i+1]] = receivedInput
+			}
+			i += 2
+		case 4:
+			outVal := getValueByMode(intArr, i+1, paramModes[0])
+			outputChannel <- outVal
+			i += 2
+		case 5:
+			if getValueByMode(intArr, i+1, paramModes[0]) > 0 {
+				i = getValueByMode(intArr, i+2, paramModes[1])
+			} else {
+				i += 3
+			}
+		case 6:
+			if getValueByMode(intArr, i+1, paramModes[0]) == 0 {
+				i = getValueByMode(intArr, i+2, paramModes[1])
+			} else {
+				i += 3
+			}
+		case 7:
+			if getValueByMode(intArr, i+1, paramModes[0]) < getValueByMode(intArr, i+2, paramModes[1]) {
+				intArr[intArr[i+3]] = 1
+			} else {
+				intArr[intArr[i+3]] = 0
+			}
+			i += 4
+		case 8:
+			if getValueByMode(intArr, i+1, paramModes[0]) == getValueByMode(intArr, i+2, paramModes[1]) {
+				intArr[intArr[i+3]] = 1
+			} else {
+				intArr[intArr[i+3]] = 0
+			}
+			i += 4
+		case 99:
+			break main
+		default:
+			panic("Invalid opcode")
+		}
+	}
 }
 
 func createIntArrFromInput(input string) []int {
