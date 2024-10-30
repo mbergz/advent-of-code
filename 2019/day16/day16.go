4614c061357cfa39cad02676904d4909cecf54a3
Martin Bergström <emarbe02@gmail.com>
2024-10-30 19:42:25 +0100

    Day16 completed

--- 2019/day16/day16.go
+++ 2019/day16/day16.go
@@ -31,22 +31,63 @@ func part1(input string) {
 		for itr := 1; itr <= len(nbrs); itr++ {
 			sum := 0
 			for i := itr - 1; i < len(nbrs); i++ {
-				newNbr := nbrs[i] * getPatternModifier(itr, i)
-				sum += leastSignificantBit(newNbr)
+				sum += nbrs[i] * getPatternModifier(itr, i)
 			}
 			nbrs[itr-1] = util.AbsInt(leastSignificantBit(sum))
 		}
 	}
 
-	for _, nbr := range nbrs {
-		fmt.Print(nbr)
+	for i := 0; i < 8; i++ {
+		fmt.Print(nbrs[i])
 	}
 	fmt.Println()
+}
+
+func convertSliceToNumber(digits []int) int {
+	number := 0
+	for _, digit := range digits {
+		number = number*10 + digit
+	}
+	return number
 }
 
 func part2(input string) {
 	defer util.Timer()()
 
+	origNbrs := make([]int, 0)
+	for _, char := range input {
+		origNbrs = append(origNbrs, int(util.ToInt(string(char))))
+	}
+
+	nbrs := make([]int, 0, len(origNbrs)*10_000)
+	for i := 0; i < 10_000; i++ {
+		nbrs = append(nbrs, origNbrs...)
+	}
+
+	offset := convertSliceToNumber(nbrs[:7])
+	nbrs = nbrs[offset:]
+
+	for phase := 1; phase <= 100; phase++ {
+
+		totalSum := 0
+		// Pattern modifier is always 1 becasue of offset (thx reddit)
+		for i := 0; i < len(nbrs); i++ {
+			totalSum += nbrs[i]
+		}
+
+		offsetSum := 0
+		last := 0
+		for itr := 0; itr < len(nbrs); itr++ {
+			offsetSum += last
+			last = nbrs[itr]
+			nbrs[itr] = util.AbsInt(leastSignificantBit(totalSum - offsetSum))
+		}
+	}
+
+	for i := 0; i < 8; i++ {
+		fmt.Print(nbrs[i])
+	}
+	fmt.Println()
 }
 
 func leastSignificantBit(nbr int) int {
