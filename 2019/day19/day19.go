e9f5ab284b941aff8c40bf5f229d6c62f82c6bcb
Martin Bergström <emarbe02@gmail.com>
2024-12-04 23:01:45 +0100

    Day19 completed

--- 2019/day19/day19.go
+++ 2019/day19/day19.go
@@ -38,4 +38,49 @@ func part1(input string) {
 func part2(input string) {
 	defer util.Timer()()
 
+	currY := 11
+	currX := 9
+	for {
+		currY++
+		prevX := currX
+		nbrLeft := getLeft(currX, currY, input)
+		nbrRight := getRight(currX, currY, input)
+		total := nbrLeft + 1 + nbrRight
+		if nbrLeft > nbrRight {
+			currX -= (nbrLeft - nbrRight) / 2
+		} else {
+			currX += (nbrRight - nbrLeft) / 2
+		}
+		if total > 100 {
+			beamMaxRightX := prevX + nbrRight
+			if checkSquare(beamMaxRightX, currY, input) {
+				fmt.Println((beamMaxRightX-99)*10000 + currY)
+				return
+			}
+		}
+	}
+
+}
+
+func checkSquare(maxX int, y int, input string) bool {
+	return intcodecomputer.InitilizeComputer(input).Run([]int{maxX - 99, y + 99})[0] == 1
+}
+
+func getLeft(x int, y int, input string) int {
+	return getCount(x, y, input, -1)
+}
+func getRight(x int, y int, input string) int {
+	return getCount(x, y, input, +1)
+}
+
+func getCount(x int, y int, input string, direction int) int {
+	count := 0
+	for {
+		x += direction
+		if intcodecomputer.InitilizeComputer(input).Run([]int{x, y})[0] == 1 {
+			count++
+		} else {
+			return count
+		}
+	}
 }
