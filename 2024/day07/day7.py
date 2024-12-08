cea74c5e8dc071075feaba958080933ca6f83204
Martin Bergström <emarbe02@gmail.com>
2024-12-08 20:09:51 +0100

    Day7 completed

--- 2024/day07/day7.py
+++ 2024/day07/day7.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 input_file = open('day7.txt', 'r')
 lines = input_file.readlines()
 
@@ -23,7 +25,28 @@ def part1():
 
 
 def part2():
-    print("count")
+    score = 0
+    for line in lines:
+        test_value = int(line.split(':')[0])
+        input_values = line.split(':')[1].strip().split(' ')
+
+        combinations = 3 ** (len(input_values) - 1)
+        for i in range(0, combinations):
+            combination = np.base_repr(i, base=3).zfill((len(input_values) - 1))
+
+            out = int(input_values[0])
+            for index, value in enumerate(input_values[1:], start=1):
+                if combination[index - 1] == '0':
+                    out = out + int(value)
+                elif combination[index - 1] == '1':
+                    out = out * int(value)
+                else:  # ||
+                    out = int(str(out) + value)
+
+            if out == test_value:
+                score += test_value
+                break
+    print(score)
 
 
 part1()
