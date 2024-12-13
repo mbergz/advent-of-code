d3deaec7d7afba0a214aeb68b08b31a2b243d054
Martin Bergström <emarbe02@gmail.com>
2024-12-13 23:06:00 +0100

    Day9 completed

--- 2024/day09/day9.py
+++ 2024/day09/day9.py
@@ -3,15 +3,7 @@ lines = input_file.readlines()
 
 
 def part1():
-    memory = []
-    block_index = 0
-    for index, char in enumerate(lines[0]):
-        if index % 2 == 0:
-            memory.extend([str(block_index)] * int(char))
-            block_index += 1
-        else:
-            memory.extend(['.'] * int(char))
-
+    memory = read_input_memory()
     left, right = 0, len(memory) - 1
     while left < right:
         if memory[left] != '.':
@@ -28,7 +20,62 @@ def part1():
 
 
 def part2():
-    print('')
+    memory = read_input_memory()
+    right = len(memory) - 1
+
+    visited = []
+    while right > 0:
+        if memory[right] == '.':
+            right -= 1
+            continue
+        start_index = right
+        nbr = memory[right]
+        while right > 1:
+            if memory[right - 1] == nbr:
+                right -= 1
+            else:
+                break
+        length = (start_index - right) + 1
+        if nbr in visited:
+            right -= 1
+            continue
+        visited.append(nbr)
+
+        # loop left to right, find matching empty space
+        count = 0
+        start_index = 0
+        for i, val in enumerate(memory):
+            if i > right:
+                break
+            if val == '.':
+                if count == 0:
+                    start_index = i
+                count += 1
+            else:
+                if count > 0:
+                    if count >= length:
+                        memory[start_index:start_index + length] = [nbr] * length
+                        memory[right:right + length] = ['.'] * length
+                        break
+                    else:
+                        count = 0
+                        start_index = 0
+        right -= 1
+
+    score = sum(i * int(val) for i, val in enumerate(memory) if val.isdigit())
+    print(score)
+
+
+def read_input_memory():
+    memory = []
+    block_index = 0
+    for index, char in enumerate(lines[0]):
+        if index % 2 == 0:
+            memory.extend([str(block_index)] * int(char))
+            block_index += 1
+        else:
+            memory.extend(['.'] * int(char))
+    return memory
 
 
 part1()
