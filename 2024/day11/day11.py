aa74130554090c285a9c87216dac7ee90df90035
Martin Bergström <emarbe02@gmail.com>
2024-12-16 22:27:03 +0100

    Day11 completed

---
+++ 2024/day11/day11.py
@@ -0,0 +1,35 @@
+import functools
+
+input_file = open('day11.txt', 'r')
+lines = input_file.readlines()
+
+
+def part1():
+    stones = [int(nbr.strip()) for nbr in lines[0].split(" ")]
+    score = sum(stone_blink(stone, 0, 25) for stone in stones)
+    print(score)
+
+
+def part2():
+    stones = [int(nbr.strip()) for nbr in lines[0].split(" ")]
+    score = sum(stone_blink(stone, 0, 75) for stone in stones)
+    print(score)
+
+
+@functools.cache
+def stone_blink(stone, itr, itr_limit):
+    if itr == itr_limit:
+        return 1
+
+    if stone == 0:
+        return stone_blink(1, itr + 1, itr_limit)
+    elif len(str(stone)) % 2 == 0:
+        length = len(str(stone))
+        return (stone_blink(int(str(stone)[:int(length / 2)]), itr + 1, itr_limit) +
+                stone_blink(int(str(stone)[int(length / 2):]), itr + 1, itr_limit))
+    else:
+        return stone_blink(stone * 2024, itr + 1, itr_limit)
+
+
+part1()
+part2()
