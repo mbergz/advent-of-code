d4fe872f3e9ddd067b3f017f16f5b0f2cadc37cd
Martin Bergström <emarbe02@gmail.com>
2024-12-30 13:11:31 +0100

    Day13 completed

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,9 +1,12 @@
 import sys
 
+import numpy as np
+
 input_file = open('day13.txt', 'r')
 lines = input_file.readlines()
 
 
+# brute force
 def part1():
     score = 0
     for i in range(0, len(lines), 4):
@@ -22,10 +25,36 @@ def part1():
     print(score)
 
 
+# linear algebra
+def part2():
+    score = 0
+    for i in range(0, len(lines), 4):
+        chunk = lines[i:i + 3]
+        a = extract_x_y(chunk[0].strip(), "+")
+        b = extract_x_y(chunk[1].strip(), "+")
+        prize = extract_prize_part2(chunk[2].strip())
+        coefficients = np.array([[a[0], b[0]], [a[1], b[1]]])
+        np_res = calc_np_round(coefficients, prize)
+        if check_solution(coefficients, np_res, prize):
+            score += int(np_res[0] * 3 + np_res[1])
+    print(score)
+
+
+def check_solution(coefficients, sol, prize):
+    result = coefficients @ sol  # matrix multiplication
+    return np.all(result == prize)
+
+
 def calc(nbr_a_press, nbr_b_press, a, b):
     new_x = nbr_a_press * a[0] + nbr_b_press * b[0]
     new_y = nbr_a_press * a[1] + nbr_b_press * b[1]
     return new_x, new_y
+
+
+def calc_np_round(coefficients, prize):
+    results = np.array([prize[0], prize[1]])
+    sol = np.linalg.solve(coefficients, results)
+    return np.rint(sol)
 
 
 def extract_x_y(line, sep):
@@ -35,8 +64,11 @@ def extract_x_y(line, sep):
     return x, y
 
 
-def part2():
-    print("score")
+def extract_prize_part2(line):
+    curr = line.strip().split(":")
+    x = int(curr[1].split(",")[0].split("=")[1]) + 10_000_000_000_000
+    y = int(curr[1].split(",")[1].split("=")[1]) + 10_000_000_000_000
+    return x, y
 
 
 part1()
