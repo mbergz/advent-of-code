6a4ec3d87b16dc335269ca7209309089be7d457c
Martin Bergström <emarbe02@gmail.com>
2025-02-02 13:31:07 +0100

    Day20 completed

--- 2024/day20/day20.py
+++ 2024/day20/day20.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from runner import PuzzleRunner
 
 directions = [
@@ -13,15 +15,32 @@ def part1(lines):
     for line in lines:
         grid.append(line.strip())
 
+    distances = get_distances(grid)
+    race_steps = len(distances) - 1
+
+    cheat_distances = build_cheat_distances(distances, grid)
+    print(sum(x <= race_steps - 100 for x in cheat_distances))
+
+
+def part2(lines):
+    grid = []
+    for line in lines:
+        grid.append(line.strip())
+
+    distances = get_distances(grid)
+    race_steps = len(distances) - 1
+
+    cheat_distances = build_cheat_distances_part2(distances, grid)
+    print(sum(x <= race_steps - 100 for x in cheat_distances))
+
+
+def get_distances(grid):
     # Run race backwards to build up map of distances to goal
     goal = find_char(grid, "E")
-
     visited = {goal}
     curr = goal
     steps = 0
-
     distances = {goal: 0}
-
     while grid[curr[1]][curr[0]] != "S":
         for d in directions:
             new_coord = (curr[0] + d[0], curr[1] + d[1])
@@ -31,11 +50,7 @@ def part1(lines):
                 steps += 1
                 distances[new_coord] = steps
                 break
-
-    race_steps = steps
-
-    cheat_distances = build_cheat_distances(distances, grid)
-    print(sum(x <= race_steps - 100 for x in cheat_distances))
+    return distances
 
 
 def build_cheat_distances(distances, grid):
@@ -59,13 +74,61 @@ def build_cheat_distances(distances, grid):
                         new_coord_cheat in distances:
                     cheat_distances.append(steps + 2 + distances[new_coord_cheat])
             else:
-                # Go forward with race trac as usual
+                # Go forward with racetrack as usual
                 visited.add(new_coord)
                 next_coord = new_coord
         curr = next_coord
         steps += 1
 
     return cheat_distances
+
+
+def build_cheat_distances_part2(distances, grid):
+    start = find_char(grid, "S")
+    visited = {start}
+    curr = start
+    cheat_distances = []
+    steps = 0
+
+    while grid[curr[1]][curr[0]] != "E":
+        next_coord = None
+
+        # Test all cheats from curr pos
+        bfs(curr, steps, distances, grid, cheat_distances)
+
+        for d in directions:
+            new_coord = (curr[0] + d[0], curr[1] + d[1])
+            if new_coord in visited:
+                continue
+            if grid[new_coord[1]][new_coord[0]] != "#":
+                # Go forward with racetrack as usual
+                visited.add(new_coord)
+                next_coord = new_coord
+        curr = next_coord
+        steps += 1
+
+    return cheat_distances
+
+
+def bfs(start, prev_steps, distances, grid, cheat_distances):
+    curr = start
+    queue = deque([(curr, 0)])
+    visited = set(curr)
+
+    while len(queue) > 0:
+        curr, steps = queue.popleft()
+        if steps > 20:
+            continue
+
+        if grid[curr[1]][curr[0]] != "#":
+            if curr in distances:
+                cheat_distances.append(prev_steps + steps + distances[curr])
+
+        for d in directions:
+            new_coord = (curr[0] + d[0], curr[1] + d[1])
+            if new_coord not in visited and is_within_bounds(new_coord, grid):
+                visited.add(new_coord)
+                queue.append((new_coord, steps + 1))
 
 
 def is_within_bounds(coord, grid):
@@ -79,4 +142,4 @@ def find_char(grid, c):
                 return col_i, row_i
 
 
-PuzzleRunner().run(part1)
+PuzzleRunner().run(part1, part2)
