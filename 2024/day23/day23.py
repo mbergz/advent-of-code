2452d5854109ec0f41f550e171b974769c946572
Martin Bergström <emarbe02@gmail.com>
2025-03-17 19:45:37 +0100

    Day23 completed

--- 2024/day23/day23.py
+++ 2024/day23/day23.py
@@ -20,4 +20,28 @@ def part1(lines):
     print(len(result))
 
 
-PuzzleRunner().run(part1)
+def part2(lines):
+    mapping = defaultdict(set)
+    for line in lines:
+        a, b = line.strip().split("-")
+        mapping[a].add(b)
+        mapping[b].add(a)
+
+    max_connected = set()
+
+    for key, value in mapping.items():
+        matching = set(value)
+        matching.add(key)
+
+        for v in value:
+            if v in matching:
+                copy_set_b = set(mapping[v])
+                copy_set_b.add(v)
+                matching = matching & copy_set_b
+
+        max_connected = matching if len(matching) > len(max_connected) else max_connected
+
+    print(",".join(sorted(max_connected)))
+
+
+PuzzleRunner().run(part1, part2)
