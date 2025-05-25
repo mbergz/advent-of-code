d7725af70b0c8c37730fe06e471e219b49e36414
Martin Bergström <emarbe02@gmail.com>
2025-05-25 20:23:55 +0200

    Day23 completed

--- 2019/day23/day23.go
+++ 2019/day23/day23.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 		panic(err)
 	}
 	part1(string(input))
+	part2(string(input))
 }
 
 func part1(input string) {
@@ -95,4 +97,158 @@ func handleOutputChannel(ctx context.Context, outputChannel chan int, inputQueue
 		}
 	}
 
+}
+
+func part2(input string) {
+	defer util.Timer()()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	nat := nat{}
+
+	var computerIos [50]*computerIo
+
+	for i := 0; i < 50; i++ {
+		inputChannel := make(chan int, 100)
+		inputChannel <- i // Assign address
+
+		computerIo := &computerIo{address: i, inputChannel: inputChannel, idle: false, outputBuffer: make([]int, 0), outputidx: 0}
+		computerIos[i] = computerIo
+	}
+
+	for i := 0; i < 50; i++ {
+		go intcodecomputer.InitilizeComputer(input).RunComputer(
+			func() int {
+				return inputFuncPart2(computerIos[i])
+			}, func(outVal int) {
+				computerIos[i].handleOutputValue(outVal, computerIos, &nat)
+			},
+			false)
+
+	}
+
+	var wgDone sync.WaitGroup
+	wgDone.Add(1)
+
+	go performNatCheck(ctx, &nat, computerIos, &wgDone)
+
+	wgDone.Wait()
+}
+
+type computerIo struct {
+	address      int
+	inputChannel chan int
+	idle         bool
+	outputBuffer []int
+	outputidx    int
+	mu           sync.Mutex
+	idleMu       sync.Mutex
+}
+
+func (ci *computerIo) isIdle() bool {
+	ci.idleMu.Lock()
+	defer ci.idleMu.Unlock()
+	return ci.idle
+}
+
+func (ci *computerIo) setIdle(value bool) {
+	ci.idleMu.Lock()
+	defer ci.idleMu.Unlock()
+	ci.idle = value
+}
+
+func (ci *computerIo) handleOutputValue(value int, computerIos [50]*computerIo, nat *nat) {
+	ci.mu.Lock()
+	defer ci.mu.Unlock()
+
+	ci.outputBuffer = append(ci.outputBuffer, value)
+	if len(ci.outputBuffer) == 3 {
+		address := ci.outputBuffer[0]
+		x := ci.outputBuffer[1]
+		y := ci.outputBuffer[2]
+
+		if address == 255 {
+			nat.set(x, y)
+		} else {
+			target := computerIos[address]
+			target.setIdle(false)
+
+			target.inputChannel <- x
+			target.inputChannel <- y
+		}
+
+		ci.outputBuffer = ci.outputBuffer[:0]
+		ci.outputidx = 0
+	}
+	ci.outputidx++
+}
+
+type nat struct {
+	x  int
+	y  int
+	mu sync.Mutex
+}
+
+func (n *nat) set(x, y int) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	n.x, n.y = x, y
+}
+
+func (n *nat) get() (int, int) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	return n.x, n.y
+}
+
+func inputFuncPart2(compIo *computerIo) int {
+	compIo.mu.Lock()
+	defer compIo.mu.Unlock()
+
+	select {
+	case val := <-compIo.inputChannel:
+		compIo.setIdle(false)
+		return val
+	default:
+		compIo.setIdle(true)
+		return -1
+	}
+}
+
+func performNatCheck(ctx context.Context, nat *nat, compIos [50]*computerIo, wg *sync.WaitGroup) {
+	seenYElements := make(map[int]bool)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			time.Sleep(10 * time.Millisecond)
+
+			allEmpty := true
+			for i := 0; i < 50; i++ {
+				if !compIos[i].isIdle() {
+					allEmpty = false
+					break
+				}
+			}
+			if allEmpty {
+				x, y := nat.get()
+
+				compIos[0].setIdle(false)
+				compIos[0].inputChannel <- x
+				compIos[0].inputChannel <- y
+
+				if _, ok := seenYElements[y]; ok {
+					fmt.Println("Second time Y from NAT to address 0 comp is", y)
+					wg.Done()
+					return
+				} else {
+					seenYElements[y] = true
+				}
+
+			}
+		}
+	}
 }
