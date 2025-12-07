cf382ec1e94878eed7556586369663c00ec6df11
Martin Bergström <emarbe02@gmail.com>
2025-12-07 13:30:56 +0100

    Day5 completed

--- 2025/src/main/kotlin/Day05.kt
+++ 2025/src/main/kotlin/Day05.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 class Day05 : PuzzleRunner() {
     override fun part1(input: List<String>) {
         val (ranges, ids) = input.filter(String::isNotBlank).partition { it.contains("-") }
@@ -11,6 +13,32 @@ class Day05 : PuzzleRunner() {
         println(res)
     }
 
+    override fun part2(input: List<String>) {
+        val ranges = input
+            .filter { it.contains("-") }
+            .map { Pair(it.split("-")[0].toLong(), it.split("-")[1].toLong()) }
+            .toList()
+            .sortedBy { it.first }
+
+        val mergedRanges = mutableListOf<Pair<Long, Long>>()
+
+        var i = 0
+        while (i < ranges.size) {
+            val start = ranges[i].first
+            var end = ranges[i].second
+
+            while (i < ranges.size - 1 && ranges[i + 1].first <= end) {
+                end = max(end, ranges[i + 1].second)
+                i++
+            }
+
+            mergedRanges.add(Pair(start, end))
+            i++
+        }
+
+        val res = mergedRanges.fold(0L) { acc, pair -> acc + ((pair.second - pair.first) + 1) }
+        println(res)
+    }
 }
 
 
